f84bc2d3030576e62d24c5a519a738591154aa15
Glenpl <kubamolinski@gmail.com>
2015-03-31 19:38:04 +0200

    i added a header file

--- main.cpp
+++ main.cpp
@@ -6,79 +6,8 @@
 #include <conio.h>
 #include <sstream>
 #include <string>
-
-using namespace std;
-
-typedef unsigned int UI;
-
-const UI BOARD_SIZE = 9; // board is a grid of x * x fields
-const UI BOMBS = 18; // number of bombs
-const bool SHOW_ZEROES = true; // show all zero-valued fields
-// special values of fields
-const UI FIELD_CLEAR = 0;
-const UI FIELD_BOMB = 9;
-const UI FIELD_MARKED = 10;
-const UI FIELD_SHOWN = 11;
-const UI FIELD_HIDDEN = 12;
-
-struct validated_input
-{
-    string action;
-    int row;
-    int col;
-};
-
-typedef validated_input val_input;
-
-// boards
-string board[BOARD_SIZE][BOARD_SIZE]; // board that will be displayed to user
-UI board_int[BOARD_SIZE][BOARD_SIZE]; // board with actual fields' values
-UI shown[BOARD_SIZE][BOARD_SIZE]; // board with values: marked, hidden, shown for each field
-
-// some global variables... :(
-UI flags = 0;
-bool lose = false;
-UI hidden_fields = BOARD_SIZE * BOARD_SIZE;
-
-// function index
-void start_game();
-void update_board();
-void update_field(UI, UI);
-UI count_bombs_around_field(int, int);
-void print_board();
-void print_board_main_body();
-void print_board_upper_body();
-void init_game();
-void clear_boards();
-void clear_field(UI, UI);
-void set_bombs_on_board();
-void set_fields_values();
-void set_field_value(UI, UI);
-void do_action(string, UI, UI);
-void mark_field(UI, UI);
-void finish_game(bool);
-void continue_or_end();
-val_input validate_input(string, string, string);
-string to_string(int);
-int to_string(string);
-
-// convert int to string
-template <class T>
-inline std::string to_string(const T& t)
-{
-    std::stringstream ss;
-    ss << t;
-    return ss.str();
-}
-
-template <class T>
-inline int to_int(const T& str)
-{
-    int i;
-    istringstream iss(str);
-    iss >> i;
-    return i;
-}
+// the header with variable and function declarations etc
+#include "main.h"
 
 void print_board_upper_body()
 {
